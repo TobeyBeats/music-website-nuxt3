47545c21d38e66d3e9feb416c72a4c42ae54874c
Tobias.Federmann <tofe00001@stud.uni-saarland.de>
2023-11-17 20:06:40 +0100

    added Popover component

---
+++ components/Popover.vue
@@ -0,0 +1,22 @@
+<template>
+	<Popover style="position: relative; display: inline;">
+		<PopoverButton class="link">
+			<slot />
+		</PopoverButton>
+
+		<PopoverPanel class="popover-body dropdown-box box-shadow-default">
+			<slot name="body" />
+		</PopoverPanel>
+	</Popover>
+</template>
+
+
+<script setup lang="ts">
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+</script>
+
+<style>
+.popover-body {
+	position: absolute;
+}
+</style>
