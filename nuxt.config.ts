3b87f123e88b9dee30f174c106a8d4cbf5826fb9
Tobias.Federmann <tofe00001@stud.uni-saarland.de>
2023-12-31 15:05:10 +0100

    added prerender routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,30 @@ export default defineNuxtConfig({
 			ownerEmailAddress: 'contact@tobeybeats.com'
 		}
 	},
+
+	nitro: {
+		prerender: {
+			routes: [
+				"/releases/down-on-the-road",
+				"/releases/do-it-now",
+				"/releases/broken-reactor-remix",
+				"/releases/lose-my-mind",
+				"/releases/lost-in-the-dark-remix",
+				"/releases/get-it-up",
+				"/releases/rescue-me",
+				"/releases/rescue-me-remixes-vol1",
+				"/releases/rescue-me-remixes-vol2",
+				"/releases/helium-remix",
+				"/releases/alone-in-the-dark",
+				"/releases/take-me-away",
+				"/releases/karma",
+				"/releases/sad-songs",
+				"/releases/pariah-remix",
+				"/releases/breakdown",
+				"/releases/under-water",
+				"/releases/sad-songs-remixes",
+				"/releases/fight-alone"
+			]
+		}
+	},
 })
