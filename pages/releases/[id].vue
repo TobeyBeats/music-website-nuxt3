4de8ab82b7e0b3121c872175a05f1caee8a00709
Tobias Federmann <tobias@40bacd56-3fd3-4f39-a398-e9528614c664.fritz.box>
2024-04-17 17:23:00 +0200

    made contributor links on release page target _blank

--- pages/releases/[id].vue
+++ pages/releases/[id].vue
@@ -63,10 +63,10 @@
 							<p>{{ participant.roles }}</p>
 						</div>
 						<p>
-							<span v-if="participant.contributor.links.instagram"><a class="link" :href="participant.contributor.links.instagram">Instagram&nbsp;↗&#xFE0E;</a> </span>&ZeroWidthSpace;
-							<span v-if="participant.contributor.links.youtube"><a class="link" :href="participant.contributor.links.youtube">Youtube&nbsp;↗&#xFE0E;</a> </span>&ZeroWidthSpace;
-							<span v-if="participant.contributor.links.spotify"><a class="link" :href="participant.contributor.links.spotify">Spotify&nbsp;↗&#xFE0E;</a> </span>&ZeroWidthSpace;
-							<span v-if="participant.contributor.links.website"><a class="link" :href="participant.contributor.links.website">Website&nbsp;↗&#xFE0E;</a></span>
+							<span v-if="participant.contributor.links.instagram"><a class="link" target="_blank" :href="participant.contributor.links.instagram">Instagram&nbsp;↗&#xFE0E;</a> </span>&ZeroWidthSpace;
+							<span v-if="participant.contributor.links.youtube"><a class="link" target="_blank" :href="participant.contributor.links.youtube">Youtube&nbsp;↗&#xFE0E;</a> </span>&ZeroWidthSpace;
+							<span v-if="participant.contributor.links.spotify"><a class="link" target="_blank" :href="participant.contributor.links.spotify">Spotify&nbsp;↗&#xFE0E;</a> </span>&ZeroWidthSpace;
+							<span v-if="participant.contributor.links.website"><a class="link" target="_blank" :href="participant.contributor.links.website">Website&nbsp;↗&#xFE0E;</a></span>
 						</p>
 					</div>
 				</div>
