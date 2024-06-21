e7186faacab9c21fa8e2bc7b11a54c2499969f73
Tobias Federmann <tobias@MacBook-Pro-von-Tobias-2.fritz.box>
2024-06-21 17:15:26 +0200

    adjust footer height responsively

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,9 +14,19 @@
 	--max-content-width: 1420px;
 	--space-sides: 50px;
 	--navbar-height: 80px;
+	--footbar-height: 80px;
+
+	@media screen and (max-width: 550px) {
+		--footbar-height: 100px;
+	}
+
+	@media screen and (max-width: 440px) {
+		--footbar-height: 120px;
+	}
 
 	@media screen and (max-width: 380px) {
 		--space-sides: 35px;
+		--footbar-height: 140px;
 	}
 
 	@media screen and (max-width: 330px) {
