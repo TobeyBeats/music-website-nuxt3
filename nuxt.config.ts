0c8bf835038a9c9ec76c58b514b1dde267423454
Tobias Federmann <tobias@MacBook-Pro-von-Tobias-2.fritz.box>
2024-06-21 17:25:29 +0200

    add feel alive and big room bang prerender routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,6 +46,8 @@ export default defineNuxtConfig({
 				"/releases/4am",
 				"/releases/problems",
 				"/releases/riders-of-the-storm",
+				"/releases/feel-alive",
+				"/releases/big-room-bang",
 			]
 		}
 	},
