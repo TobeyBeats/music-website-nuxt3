6f2c398f459f1dd6a9d20812c7a449aca33e6966
Tobias Federmann <tobias@MacBook-Pro-von-Tobias-2.fritz.box>
2024-07-19 12:09:30 +0200

    put fight alone on home page

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@ const colorsDefault: [string, string] = [constants.colorsDefault[0], constants.c
 
 const config = useRuntimeConfig()
 
-const { data: data1, error: error1 } = await useFetch<ConstructorParameters<typeof Release>[0]>("/releases/fight-alone", {
+const { data: data1, error: error1 } = await useFetch<ConstructorParameters<typeof Release>[0]>("/releases/feel-alive", {
 	baseURL: config.public.baseUrlApi
 })
 
