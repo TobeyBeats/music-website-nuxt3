d57afe34d55bf26e613e1c0d6c3127fbd538aa0a
Tobias Federmann <tobias@MBPvonTobias2.fritz.box>
2024-08-09 11:10:32 +0200

    add greedy to home page

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@ const colorsDefault: [string, string] = [constants.colorsDefault[0], constants.c
 
 const config = useRuntimeConfig()
 
-const { data: data1, error: error1 } = await useFetch<ConstructorParameters<typeof Release>[0]>("/releases/big-room-bang", {
+const { data: data1, error: error1 } = await useFetch<ConstructorParameters<typeof Release>[0]>("/releases/greedy", {
 	baseURL: config.public.baseUrlApi
 })
 
