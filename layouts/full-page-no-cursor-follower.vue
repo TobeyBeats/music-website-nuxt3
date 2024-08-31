b3ffa26af7542f56b9bd663ee01cc226a4fe7f14
Tobias Federmann <tobias@c03689d6-60e0-42c4-9538-ee024f90a9b3.fritz.box>
2024-08-31 11:38:37 +0200

    make no cursor layout with nested style similar to default

--- layouts/full-page-no-cursor-follower.vue
+++ layouts/full-page-no-cursor-follower.vue
@@ -1,19 +1,43 @@
 <template>
-	<div>
-		<Navbar/>
-		<main>
-			<slot/>
-		</main>
-		<Footbar />
-	</div>
+    <div id="layout-full-page-no-cursor-follower">
+        <Navbar/>
+        <main>
+            <slot/>
+        </main>
+        <Footbar />
+    </div>
 </template>
 
-<style scoped>
-main {
-	max-width: 100%;
-	padding-inline: 0px;
-	margin-top: calc(var(--navbar-height) + var(--space-sides));
-	margin-inline: 0px;
-	padding-bottom: calc(var(--navbar-height) + var(--space-sides));
+<style lang="scss" scoped>
+#layout-full-page-no-cursor-follower {
+    --max-content-width: 1420px;
+    --space-sides: 50px;
+    --navbar-height: 80px;
+    --footbar-height: 80px;
+
+    @media screen and (max-width: 550px) {
+        --footbar-height: 100px;
+    }
+
+    @media screen and (max-width: 440px) {
+        --footbar-height: 120px;
+    }
+
+    @media screen and (max-width: 380px) {
+        --space-sides: 35px;
+        --footbar-height: 140px;
+    }
+
+    @media screen and (max-width: 330px) {
+        --space-sides: 30px;
+    }
+
+    main {
+        max-width: 100%;
+        padding-inline: 0px;
+        margin-top: calc(var(--navbar-height) + var(--space-sides));
+        margin-inline: 0px;
+        padding-bottom: calc(var(--footbar-height) + var(--space-sides));
+    }
 }
 </style>
